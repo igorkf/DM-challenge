a4030858887a00eb4fb24f18e10d3c38e546a1d2
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-06-19 15:19:19 -0300

    fixed pydantic model and template logic

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
+from pathlib import Path
+import json
+
 from fastapi import FastAPI, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 from .problema_1 import fibo
-from .problema_2 import plataformas, Transporte
+from .problema_2 import Transporte
 
+
+BASE_DIR = Path(__file__).parent.parent.absolute()
 
 app = FastAPI()
+app.mount(
+    '/static',
+    StaticFiles(directory=BASE_DIR / 'app/static'),
+    name='static'
+)
 templates = Jinja2Templates(directory='app/templates')
 
 
@@ -31,16 +42,37 @@ async def fibonacci(n: int):
 
 
 @app.get('/transporte', response_class=HTMLResponse)
-async def show_vehicle_options(request: Request, model: Transporte = Depends()):
-    for x in plataformas:
-        if model.largura <= x['largura_max'] and model.altura <= x['altura_max'] and model.espessura <= x['espessura_max'] and model.peso <= x['peso_max']:
-            x['possui_requisitos'] = 'Sim'
-        else:
-            x['possui_requisitos'] = 'Não'
+async def transporte(request: Request, model: Transporte = Depends()):
+    '''
+    Calcula quais opções de transporte suportam uma mercadoria de acordo
+    com as características da mercadoria, além de mostrar o custo benefício.   
+    '''
+
+    with open('app/problema_2/data.json', 'r') as f:
+        data = json.loads(f.read())
+
+    for x in data:
+        x['volume_max'] = x['largura_max'] * \
+            x['altura_max'] * x['espessura_max']
+        x['possui_requisitos'] = 'Não'
+        x['nivel_de_custo'] = None
+
+    sorted_data = sorted(data, key=lambda k: (
+        k['plataforma'], k['volume_max']))
+    plataformas = sorted({x['plataforma'] for x in sorted_data})
+
+    for plataforma in plataformas:
+        i = 0
+        for x in sorted_data:
+            if x['plataforma'] == plataforma and model.largura <= x['largura_max'] and model.altura <= x['altura_max'] and model.espessura <= x['espessura_max'] and model.peso <= x['peso_max']:
+                x['possui_requisitos'] = 'Sim'
+                i += 1
+                x['nivel_de_custo'] = i
 
     context = {
         'request': request,
         'plataformas': plataformas,
+        'data': sorted_data,
         'model': model
     }
 
--- app/problema_2/__init__.py
+++ app/problema_2/__init__.py
@@ -1,61 +1,8 @@
-from typing import Union, Optional
-
 from pydantic import BaseModel
 
 
-plataformas = [
-    {
-        'plataforma': 'Lala',
-        'veiculo': 'Moto',
-        'largura_max': 35,
-        'altura_max': 40,
-        'espessura_max': 30,
-        'peso_max': 20
-    },
-    {
-        'plataforma': 'Lala',
-        'veiculo': 'Fiorino',
-        'largura_max': 188,
-        'altura_max': 133,
-        'espessura_max': 108,
-        'peso_max': 500
-    },
-    {
-        'plataforma': 'Lala',
-        'veiculo': 'Carreto',
-        'largura_max': 300,
-        'altura_max': 180,
-        'espessura_max': 200,
-        'peso_max': 1500
-    },
-    {
-        'plataforma': 'Ogi',
-        'veiculo': 'Moto',
-        'largura_max': 52,
-        'altura_max': 36,
-        'espessura_max': 52,
-        'peso_max': 20
-    },
-    {
-        'plataforma': 'Ogi',
-        'veiculo': 'SUV',
-        'largura_max': 125,
-        'altura_max': 80,
-        'espessura_max': 60,
-        'peso_max': 200
-    }
-]
-
-for x in plataformas:
-    x['volume_max'] = x['largura_max'] * x['altura_max'] * x['espessura_max']
-
-plataformas = sorted(plataformas, key=lambda x: x['volume_max'])
-
-
 class Transporte(BaseModel):
-    plataforma: Optional[str]
-    veiculo: Optional[str]
-    largura: Union[int, float]
-    altura: Union[int, float]
-    espessura: Union[int, float]
-    peso: Union[int, float]
+    largura: float
+    altura: float
+    espessura: float
+    peso: float
